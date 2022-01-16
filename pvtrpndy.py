709db50a361b55cac183df9e548d82c46080f63d
Pavitra <pandeypavitra65@gmail.com>
2022-01-16 16:23:35 +0530

    task 3 completed

--- pvtrpndy.py
+++ pvtrpndy.py
@@ -1,3 +1,3 @@
 print("Black mirror")
 print("Depiction technology in future and its ill and bizzare uses")
-print("Learned all the concepts of GIT perfectly")
+print("Learned all the concepts of git perfectly")
