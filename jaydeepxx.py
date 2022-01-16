0750eacf12c3f0fe44633abb4821ac7c5d1819ef
Jaydeep <jaydeepmeena62@gmail.com>
2022-01-16 16:22:29 +0530

    Commit <Question No. 3>

--- jaydeepxx.py
+++ jaydeepxx.py
@@ -1,2 +1,3 @@
 print("Answer Of First Question Is - Mr. Robot")
 print("Answer of 2nd question is - Because hacking always facinates me and in the show the hacking which is showing is real")
+print("Answer of 3rd question is - No One Sorry.. I'm Kind Of Introvert.. But Now I'm Thinking About It :)")
