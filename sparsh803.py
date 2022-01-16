18ed52c3e33a6804ead2a70182d73da64d3f8735
sparsh803 <sparshg2003@gmail.com>
2022-01-16 14:11:16 +0400

    Commit Message changed successfully

--- sparsh803.py
+++ sparsh803.py
@@ -1,3 +1,3 @@
 ans1) suits
 ans2) i like it because of its witty characters.
-ans3) Ananya (well played seniors , but thats all you will ever know)
+ans5) had a pretty good experience attending adn interacting in the classesm, the small tasks gi en after the classes were fun to do.
