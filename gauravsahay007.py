0659b9419bcf5fe512a6ea9da1319502f825cd60
gauravsahay007 <gauravsahay2468@gmail.com>
2022-01-16 15:52:49 +0530

    task3 completed

---
+++ gauravsahay007.py
@@ -0,0 +1,9 @@
+from hashlib import sha3_224
+from xml.dom.minidom import CharacterData
+from xml.sax import SAXNotRecognizedException
+
+
+ques1: The Family Man S1 & S2
+ques2: Its a patriotic show and Shreekant tiwari literally was amazinng Character
+ques3: Kriti Sanon 
+ques4: Y 
