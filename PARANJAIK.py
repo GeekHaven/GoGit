a84166809e5c05864f6bf2bcf6521b0000620eb3
Paranjai <karnatipj@gmail.com>
2022-01-16 16:05:31 +0530

    Question No. 4

--- PARANJAIK.py
+++ PARANJAIK.py
@@ -1,3 +1,7 @@
+from tkinter import Y
+
+
 1. My favourite tv show is "MEELO EVARU KOTEESHWARUDU"
 2. I like this show because it gives a lot of general knowledge with entertainment
 3. Sneha(outside college)
+4. Y
