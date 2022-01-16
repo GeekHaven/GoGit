8db54b9e55921d07e34320ca66ba0ac55912fa62
Jaydeep <jaydeepmeena62@gmail.com>
2022-01-16 16:21:09 +0530

    Commit Message changed successfully

--- jaydeepxx.py
+++ jaydeepxx.py
@@ -1,1 +1,3 @@
 print("Answer Of First Question Is - Mr. Robot")
+print("Answer of 2nd question is - Because hacking always facinates me and in the show the hacking which is showing is real")
+print("Answer of 5th questionis - it was amazing ")
