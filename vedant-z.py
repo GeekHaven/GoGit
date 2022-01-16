7501cba950225d0ce00314886eca6385f8462884
Vedant Borkar <vedantborkar1234@gmail.com>
2022-01-16 16:48:28 +0530

    Update vedant-z.py
    removed barriers

--- vedant-z.py
+++ vedant-z.py
@@ -1,8 +1,5 @@
 Chernobyl
-<<<<<<< HEAD
-I like it as it was about One of the most devastating NUCLEAR DISASTER IN HISTORY.It was all shown so nicely.
-=======
 I like it as it was about One of the most devastating NUCLEAR DISASTER IN HISTORY.It was all shown so nicely.
 Arpita
 N
->>>>>>> d9f9c135adc33e304d5f93646c2fd64b36defc42
+
