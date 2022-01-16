646723de250ba0efb7c38a16c18de883cb85921f
sparsh803 <sparshg2003@gmail.com>
2022-01-16 14:16:49 +0400

    Task 3 completed

--- sparsh803.py
+++ sparsh803.py
@@ -1,3 +1,4 @@
 ans1) suits
 ans2) i like it because of its witty characters.
+
 ans5) had a pretty good experience attending adn interacting in the classesm, the small tasks gi en after the classes were fun to do.
